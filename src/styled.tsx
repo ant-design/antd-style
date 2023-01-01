d9c9756084a88d12c99876768e59b182ec122fab
arvinxx <arvinx@foxmail.com>
2023-01-02 02:52:42 +0800

    :white_check_mark: test: 补充 styled 测试类型的用例

--- src/styled.tsx
+++ src/styled.tsx
@@ -1,6 +1,7 @@
 // ===========  styled 方案  =========== //
 // 适用场景：看上去像是一个自定义样式组件的场景，且可能会有相应的入参
 // 推荐使用 styled 将其包裹为一个样式组件
+
 import { Theme, ThemeProvider as Provider } from '@emotion/react';
 import { FC, memo, PropsWithChildren } from 'react';
 
@@ -14,15 +15,21 @@ declare module '@emotion/react' {
 }
 
 export interface ThemeProviderProps {
+  /**
+   * 自定义 token， 可在 antd v5 token 规范基础上扩展和新增自己需要的 token
+   */
   customToken?: Record<string, any>;
+  /**
+   * 自定义 stylish 可以自行扩展和新增自己需要的符合样式
+   */
   customStylish?: Record<string, string>;
 }
 
 export const ThemeProvider: FC<PropsWithChildren<ThemeProviderProps>> = memo(
-  ({ children, customToken, customStylish }) => {
+  ({ children, customToken = {}, customStylish = {} }) => {
     const token = useToken();
 
-    const stylish = customStylish ?? {};
+    const stylish = { ...customStylish };
     const theme: Theme = { ...token, ...customToken, stylish };
 
     return <Provider theme={theme}>{children}</Provider>;
--- tests/styled.test.tsx
+++ tests/styled.test.tsx
@@ -11,7 +11,7 @@ describe('styled', () => {
     expect(App).toBeTruthy();
   });
   describe('使用 antd 主题样式', () => {
-    it('没有包裹 Provider 的情况下，找不到样式', () => {
+    it('没有包裹 ThemeProvider 的情况下，找不到样式', () => {
       const App = styled.div`
         width: 30px;
         color: ${(p) => p.theme.colorPrimary};
@@ -23,22 +23,49 @@ describe('styled', () => {
       expect(container).toMatchSnapshot();
     });
 
-    it('在 Provider 包裹下，能正常获得样式', () => {
+    it('在 ThemeProvider 包裹下，能正常获得样式', () => {
       const App = styled.div`
         width: 30px;
         color: ${(p) => p.theme.colorPrimary};
         background: ${(p) => p.theme.colorPrimaryBg};
       `;
 
-      const { container } = render(
-        <ThemeProvider>
-          <App>自定义样式</App>
-        </ThemeProvider>,
-      );
+      const { container } = render(<App>自定义样式</App>, { wrapper: ThemeProvider });
       expect(container.firstChild).toHaveStyle({
         width: '30px',
         color: '#1677ff',
         background: '#e6f4ff',
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('带有外部传参的方式', () => {
+      const Card = styled.div<{ primary?: boolean }>`
+        border-radius: ${(p) => p.theme.borderRadiusLG}px;
+        padding: ${(p) => p.theme.paddingLG}px;
+
+        background: ${(p) => (p.primary ? p.theme.colorPrimary : p.theme.colorBgContainer)};
+        color: ${(p) => (p.primary ? p.theme.colorTextLightSolid : p.theme.colorText)};
+      `;
+      const { container, rerender } = render(<Card>卡片</Card>, { wrapper: ThemeProvider });
+
+      expect(container.firstChild).toHaveStyle({
+        borderRadius: '8px',
+        padding: '24px',
+        color: 'rgba(0,0,0,0.88)',
+        background: 'rgb(255, 255, 255)',
+      });
+
+      expect(container).toMatchSnapshot();
+
+      rerender(<Card primary>卡片</Card>);
+
+      expect(container.firstChild).toHaveStyle({
+        borderRadius: '8px',
+        padding: '24px',
+        background: '#1677ff',
+        color: 'rgb(255, 255, 255)',
       });
 
       expect(container).toMatchSnapshot();
