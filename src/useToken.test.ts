e3b4bc928b2c69ed0627e9d712c7d6703815a054
arvinxx <arvinx@foxmail.com>
2023-01-02 00:50:50 +0800

    :white_check_mark: test: add useToken unit test case

---
+++ src/useToken.test.ts
@@ -0,0 +1,12 @@
+import { renderHook } from '@testing-library/react';
+
+import { useToken } from './useToken';
+
+describe('useToken', () => {
+  it('can get colorPrimary', () => {
+    const { result } = renderHook(useToken);
+    expect(result.current.colorPrimary.toLowerCase()).toEqual('#1677ff');
+  });
+
+  it('TODO: 嵌套 CP 时能拿到准确的 token 值', () => {});
+});
