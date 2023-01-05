de9b903ca0c77dab64cbb17d5953e62cf6b3325e
arvinxx <arvinx@foxmail.com>
2023-01-06 01:59:01 +0800

    :bug: fix: 补充 Stylish Token 的类型定义

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,3 +1,4 @@
+import { AntdStylish } from '@/stylish';
 import { AliasToken } from 'antd/es/theme/interface';
 
 export type AntdToken = AliasToken;
@@ -12,7 +13,7 @@ export interface CustomStylish {}
 export interface CustomTheme extends CustomStylish, CustomToken {}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Stylish extends CustomStylish {}
+export interface Stylish extends AntdStylish, CustomStylish {}
 
 export interface FullToken extends AntdToken, CustomToken {}
 
