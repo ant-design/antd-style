2c3749cd219aa818c1bc758cae8d41c76e71c153
arvinxx <arvinx@foxmail.com>
2023-01-09 13:55:04 +0800

    :label: fix: 修正 theme props 类型定义失效的问题

--- src/containers/ThemeProvider/type.ts
+++ src/containers/ThemeProvider/type.ts
@@ -1,9 +1,13 @@
-import { GetAntdThemeConfig, GetCustomStylish, GetCustomToken } from '@/types';
+import { GetCustomStylish, GetCustomToken, ThemeAppearance } from '@/types';
 import { ThemeConfig } from 'antd/es/config-provider/context';
 import { MessageInstance } from 'antd/es/message/interface';
 import { ModalStaticFunctions } from 'antd/es/modal/confirm';
 import { NotificationInstance } from 'antd/es/notification/interface';
 import { ReactNode } from 'react';
+
+export interface GetAntdTheme {
+  (appearance: ThemeAppearance): ThemeConfig | undefined;
+}
 
 export interface ThemeProviderProps<T, S = Record<string, string>> {
   children: ReactNode;
@@ -18,7 +22,7 @@ export interface ThemeProviderProps<T, S = Record<string, string>> {
   /**
    * 直接传入 antd 主题，或者传入一个函数，根据当前的主题模式返回对应的主题
    */
-  theme?: ThemeConfig | GetAntdThemeConfig;
+  theme?: ThemeConfig | GetAntdTheme;
   prefixCls?: string;
   /**
    * 从 ThemeProvider 中获取静态方法的实例对象
