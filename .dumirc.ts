0df76162686e6579fb9b7ea186afac551194421e
arvinxx <arvinx@foxmail.com>
2023-01-20 23:48:44 +0800

    :wrench: chore: 尝试增加 emotion babel 插件

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,4 +8,5 @@ export default defineConfig({
   favicons: [
     'https://gw.alipayobjects.com/zos/hitu-asset/c88e3678-6900-4289-8538-31367c2d30f2/hitu-1609235995955-image.png',
   ],
+  extraBabelPlugins: ['@emotion'],
 });
