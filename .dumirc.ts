a1a5f6a9fee0bc3a0e226848d0529a2cb25b57c4
arvinxx <arvinx@foxmail.com>
2023-01-22 20:04:33 +0800

    :sparkles: docs: 优化暗色模式下第一次加载时的背景色

--- .dumirc.ts
+++ .dumirc.ts
@@ -9,4 +9,11 @@ export default defineConfig({
     'https://gw.alipayobjects.com/zos/hitu-asset/c88e3678-6900-4289-8538-31367c2d30f2/hitu-1609235995955-image.png',
   ],
   extraBabelPlugins: ['@emotion'],
+  styles: [
+    `html, body { background: transparent;  }
+
+  @media (prefers-color-scheme: dark) {
+    html, body { background: #0E1116; }
+  }`,
+  ],
 });
