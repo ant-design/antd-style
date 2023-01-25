fdab53dbc16f4ee63ec9047d33831d183fa73276
arvinxx <arvinx@foxmail.com>
2023-01-25 18:33:04 +0800

    :white_check_mark: test: 补充 useResponsive hook 单测

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -19,6 +19,8 @@ Object.defineProperty(window, 'matchMedia', {
     matches: false,
     media: query,
     onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
