1a4b9f9528c64c4677874f42859780b955efb24f
arvinxx <arvinx@foxmail.com>
2023-01-25 19:05:10 +0800

    :art: chore: 优化文档代码

--- .dumi/theme/layouts/DocLayout/styles.ts
+++ .dumi/theme/layouts/DocLayout/styles.ts
@@ -8,20 +8,17 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   @font-face {
-    font-weight: normal;
     font-family: AliPuHui;
+    font-weight: normal;
     src: url('//at.alicdn.com/t/webfont_exesdog9toj.woff2') format('woff2'),
     url('//at.alicdn.com/t/webfont_exesdog9toj.woff') format('woff'),
-      /* chrome、firefox */ url('//at.alicdn.com/t/webfont_exesdog9toj.ttf')
-    format('truetype'); /* chrome、firefox、opera、Safari, Android, iOS 4.2+ */
+    url('//at.alicdn.com/t/webfont_exesdog9toj.ttf') format('truetype');
   }
 
   @font-face {
-    font-weight: bold;
     font-family: AliPuHui;
-    src: url('https://at.alicdn.com/wf/webfont/exMpJIukiCms/Gsw2PSKrftc1yNWMNlXgw.woff2')
-    format('woff2'),
-    url('https://at.alicdn.com/wf/webfont/exMpJIukiCms/vtu73by4O2gEBcvBuLgeu.woff')
-    format('woff');
+    font-weight: bold;
+    src: url('https://at.alicdn.com/wf/webfont/exMpJIukiCms/Gsw2PSKrftc1yNWMNlXgw.woff2') format('woff2'),
+    url('https://at.alicdn.com/wf/webfont/exMpJIukiCms/vtu73by4O2gEBcvBuLgeu.woff') format('woff');
   }
 `;
