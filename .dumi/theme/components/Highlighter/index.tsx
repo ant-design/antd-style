eb6a7168114db88d22596e54f8991a3cbe98047d
arvinxx <arvinx@foxmail.com>
2023-01-25 21:36:07 +0800

    :memo: docs: 优化文档构建产物

--- .dumi/theme/components/Highlighter/index.tsx
+++ .dumi/theme/components/Highlighter/index.tsx
@@ -1,7 +1,21 @@
 import { useThemeMode } from 'antd-style';
 import { memo } from 'react';
-import { default as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomOneDark, githubGist } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/hljs/javascript';
+import json from 'react-syntax-highlighter/dist/cjs/languages/hljs/json';
+import less from 'react-syntax-highlighter/dist/cjs/languages/hljs/less';
+import markdown from 'react-syntax-highlighter/dist/cjs/languages/hljs/markdown';
+import typescript from 'react-syntax-highlighter/dist/cjs/languages/hljs/typescript';
+
+SyntaxHighlighter.registerLanguage('javascript', javascript);
+SyntaxHighlighter.registerLanguage('jsx', javascript);
+SyntaxHighlighter.registerLanguage('json', json);
+SyntaxHighlighter.registerLanguage('markdown', markdown);
+SyntaxHighlighter.registerLanguage('less', less);
+SyntaxHighlighter.registerLanguage('typescript', typescript);
+SyntaxHighlighter.registerLanguage('tsx', typescript);
 
 export interface HighlighterProps {
   children: string;
--- .dumi/theme/layouts/DocLayout/styles.ts
+++ .dumi/theme/layouts/DocLayout/styles.ts
@@ -13,6 +13,7 @@ export const GlobalStyle = createGlobalStyle`
     src: url('//at.alicdn.com/t/webfont_exesdog9toj.woff2') format('woff2'),
     url('//at.alicdn.com/t/webfont_exesdog9toj.woff') format('woff'),
     url('//at.alicdn.com/t/webfont_exesdog9toj.ttf') format('truetype');
+    font-display: swap;
   }
 
   @font-face {
@@ -20,5 +21,6 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: bold;
     src: url('https://at.alicdn.com/wf/webfont/exMpJIukiCms/Gsw2PSKrftc1yNWMNlXgw.woff2') format('woff2'),
     url('https://at.alicdn.com/wf/webfont/exMpJIukiCms/vtu73by4O2gEBcvBuLgeu.woff') format('woff');
+    font-display: swap;
   }
 `;
