8456f9099e471d49d290d21064f423012304e4f4
arvinxx <arvinx@foxmail.com>
2023-02-06 19:51:44 +0800

    :memo: docs: 增加 styled 写法案例

--- docs/case/styled.md
+++ docs/case/styled.md
@@ -4,3 +4,7 @@ group: 自定义主题
 ---
 
 # 使用 Styled 构建风格样式组件
+
+## 风格化标题组件
+
+<code src="../demos/cases/Typography/default.tsx"></code>
--- docs/demos/styled/basic.tsx
+++ docs/demos/styled/basic.tsx
@@ -1,42 +1,7 @@
-import { SettingOutlined, SmileOutlined } from '@ant-design/icons';
-import { styled } from 'antd-style';
-
-const Icon = styled.span`
-  display: flex;
-  flex: 1;
-  color: red;
-`;
-
-const ButtonCtn = styled.button`
-  background: dodgerblue;
-  color: white;
-  border: ${Math.random()}px solid white;
-
-  &:focus,
-  &:hover {
-    padding: 1em;
-  }
-
-  .otherClass {
-    margin: 0;
-  }
-
-  ${Icon} {
-    color: black;
-  }
-`;
+import DsAvatar from '../common/Typography/Avatar';
 
 export default () => (
   <div>
-    <Icon>
-      <SmileOutlined />
-    </Icon>
-
-    <ButtonCtn>
-      <Icon>
-        <SettingOutlined />
-      </Icon>
-      按钮
-    </ButtonCtn>
+    <DsAvatar name={'DsAvatar'} />
   </div>
 );
