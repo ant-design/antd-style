6f0102886644236b65394f952876fd4cb3ff8558
arvinxx <arvinx@foxmail.com>
2023-02-12 00:31:06 +0800

    :wrench: chore: 优化依赖关系并修正网站构建

--- packages/theme/.fatherrc.ts
+++ packages/theme/.fatherrc.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'father';
+import path from 'path';
 
 export default defineConfig({
   plugins: ['father-plugin-dumi-theme'],
+  alias: {
+    'antd-style': path.resolve(__dirname, '../../es'),
+  },
 });
