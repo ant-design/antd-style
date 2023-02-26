da488d9753bd97d0a38c91b8399838c4dbaac97b
arvinxx <arvinx@foxmail.com>
2023-02-26 18:05:13 +0800

    :memo: docs: 移除不必要的 Demo Layout

--- .dumi/theme/builtins/Previewer/index.tsx
+++
@@ -1,9 +0,0 @@
-import Previewer from 'dumi/theme-original/builtins/Previewer';
-
-import DemoProvider from '../../components/DemoProvider';
-
-export default (props: any) => (
-  <DemoProvider>
-    <Previewer {...props} />
-  </DemoProvider>
-);
--- .dumi/theme/components/DemoProvider/index.tsx
+++
@@ -1,6 +0,0 @@
-import { StyleProvider } from 'antd-style';
-import type { PropsWithChildren } from 'react';
-
-export default ({ children }: PropsWithChildren) => {
-  return <StyleProvider prefix={'demo'}>{children}</StyleProvider>;
-};
