fae104be38a32a0f59d7e33d22c7f31ba0e272e9
arvinxx <arvinx@foxmail.com>
2023-03-14 23:43:08 +0800

    :memo: docs: 优化第一个 demo 示例

--- docs/demos/createStyles/default.tsx
+++ docs/demos/createStyles/default.tsx
@@ -1,6 +1,7 @@
 /**
  * title: createStyles 标准用法
  * description: 包含了两种css书写方式，且集成了 token 的使用
+ * defaultShowCode: true
  */
 import { createStyles } from 'antd-style';
 
@@ -8,6 +9,7 @@ const useStyles = createStyles(({ token, css }) => ({
   // 支持 css object 的写法
   container: {
     backgroundColor: token.colorBgLayout,
+    borderRadius: token.borderRadiusLG,
     maxWidth: 400,
     width: '100%',
     height: 180,
@@ -17,24 +19,23 @@ const useStyles = createStyles(({ token, css }) => ({
     flexDirection: 'column',
     marginLeft: 'auto',
     marginRight: 'auto',
-    borderRadius: token.borderRadiusLG,
   },
   // 也支持通过 css 字符串模板获得和 普通 css 一致的书写体验
   card: css`
+    color: ${token.colorTextTertiary};
     box-shadow: ${token.boxShadow};
+    &:hover {
+      color: ${token.colorTextSecondary};
+      box-shadow: ${token.boxShadowSecondary};
+    }
+
     padding: ${token.padding}px;
     border-radius: ${token.borderRadius}px;
-    color: ${token.colorTextTertiary};
     background: ${token.colorBgContainer};
     transition: all 100ms ${token.motionEaseInBack};
 
     margin-bottom: 8px;
     cursor: pointer;
-
-    &:hover {
-      color: ${token.colorTextSecondary};
-      box-shadow: ${token.boxShadowSecondary};
-    }
   `,
 }));
 
